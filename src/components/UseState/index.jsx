ad4da3df40cee68f250ac9f4d82cccc60b97e95b
Danniel Navas <dannielnavas@gmail.com>
2024-05-24 07:52:39 -0500

    ✨ chore: Add loading state and error handling to UseState component

--- src/components/UseState/index.jsx
+++ src/components/UseState/index.jsx
@@ -1,15 +1,27 @@
 /* eslint-disable react/prop-types */
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 const UseState = ({ name }) => {
   const [error, setError] = useState(false);
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    if (loading) {
+      setTimeout(() => {
+        setError(true);
+        setLoading(false);
+      }, 2000);
+    }
+  }, [loading]);
+
   return (
     <div>
       <h1>Eliminar {name}</h1>
       <p>Por favor escribe el código de seguridad</p>
       {error && <p> Error el código es incorrecto.</p>}
+      {loading && <p>Cargando...</p>}
       <input type="text" placeholder="Código de seguridad" />
-      <button onClick={() => setError(!error)}>Comprobar</button>
+      <button onClick={() => setLoading(!loading)}>Comprobar</button>
     </div>
   );
 };
