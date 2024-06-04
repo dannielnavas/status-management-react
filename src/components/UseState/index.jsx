a51562ee9a263f337488c3b63958a27e12c855db
Danniel Navas <dannielnavas@gmail.com>
2024-06-04 07:27:25 -0500

    ✨ feat: Add security code validation and loading state to UseState component

--- src/components/UseState/index.jsx
+++ src/components/UseState/index.jsx
@@ -1,16 +1,23 @@
 /* eslint-disable react/prop-types */
 import { useEffect, useState } from "react";
 
+const SECURITY_CODE = "paradigma";
+
 const UseState = ({ name }) => {
   const [error, setError] = useState(false);
   const [loading, setLoading] = useState(false);
+  const [value, setValue] = useState("");
 
   useEffect(() => {
     if (loading) {
       // si loading es true entonces se ejecuta el setTimeout
       setTimeout(() => {
-        setError(true);
         setLoading(false);
+        if (value !== SECURITY_CODE) {
+          setError(true);
+          return;
+        }
+        setError(false);
       }, 2000);
     }
   }, [loading]); // valida cada vez que cambia loading
@@ -19,10 +26,15 @@ const UseState = ({ name }) => {
     <div>
       <h1>Eliminar {name}</h1>
       <p>Por favor escribe el código de seguridad</p>
-      {error && <p> Error el código es incorrecto.</p>}
+      {error && !loading && <p> Error el código es incorrecto.</p>}
       {loading && <p>Cargando...</p>}
-      <input type="text" placeholder="Código de seguridad" />
-      <button onClick={() => setLoading(!loading)}>Comprobar</button>
+      <input
+        type="text"
+        value={value}
+        onChange={(event) => setValue(event.target.value)}
+        placeholder="Código de seguridad"
+      />
+      <button onClick={() => setLoading(true)}>Comprobar</button>
     </div>
   );
 };
