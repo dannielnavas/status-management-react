ffd95297088515b27f7d0bd34ba137f9cfafbc6e
Danniel Navas <dannielnavas@gmail.com>
2024-06-04 07:27:18 -0500

    ✨ feat: Add security code validation and loading state to ClassState component

--- src/components/ClassState/index.jsx
+++ src/components/ClassState/index.jsx
@@ -1,12 +1,43 @@
 /* eslint-disable react/prop-types */
 import { Component } from "react";
 
+const SECURITY_CODE = "paradigma";
+
 class ClassState extends Component {
   constructor() {
     super();
     this.state = {
+      value: "",
       error: false,
+      loading: false,
     };
+  }
+
+  UNSAFE_componentWillMount() {
+    console.log("ComponentWillMount");
+  }
+
+  componentDidMount() {
+    console.log("ComponentDidMount");
+  }
+
+  componentWillUnmount() {
+    console.log("ComponentWillUnmount");
+  }
+
+  componentDidUpdate() {
+    console.log("ComponentDidUpdate");
+
+    if (this.state.loading) {
+      setTimeout(() => {
+        this.setState({ loading: false });
+        if (this.state.value !== SECURITY_CODE) {
+          this.setState({ error: true });
+          return;
+        }
+        this.setState({ error: false });
+      }, 2000);
+    }
   }
 
   render() {
@@ -15,12 +46,20 @@ class ClassState extends Component {
         <h1>Eliminar {this.props.name}</h1>
         <p>Por favor escribe el código de seguridad</p>
 
-        {this.state.error && <p> Error el código es incorrecto.</p>}
+        {this.state.error && !this.state.loading && (
+          <p> Error el código es incorrecto.</p>
+        )}
+        {this.state.loading && <p>Cargando...</p>}
 
-        <input type="text" placeholder="Código de seguridad" />
-        <button onClick={() => this.setState({ error: !this.state.error })}>
-          Comprobar
-        </button>
+        <input
+          type="text"
+          placeholder="Código de seguridad"
+          value={this.state.value}
+          onChange={(event) => {
+            this.setState({ value: event.target.value });
+          }}
+        />
+        <button onClick={() => this.setState({ loading: true })}>Comprobar</button>
       </div>
     );
   }
